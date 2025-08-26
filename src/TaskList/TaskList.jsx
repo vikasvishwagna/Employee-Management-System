95f0933587eb3299bc3fad99e5aeeeda7eb92be5
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-08-26 23:57:17 +0530

    feat:added UI for TaskListNumeber & Header

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -5,15 +5,64 @@ function TaskList() {
     <div id='taskList' className='flex flex-nowrap overflow-x-auto gap-5 h-[55%] w-full  mt-25 py-6 px-1 '> 
 
       <div className='h-full w-[300px] bg-orange-300 rounded-xl flex-shrink-0 pt-2 '>
-        <div className='flex justify-between px-5'>
+        <div className='flex justify-between px-5 items-center'>
           <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
           <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
         </div>
 
-        <h2 className='text-3xl mt-10 p-5 font-bold'>Ek aur task</h2>
-        <h3 className='text-2xl  px-5 font-semibold'>Task jaisa kabhi nahi deha waisa</h3>
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
       </div>
 
+      <div className='h-full w-[300px] bg-[#ff006e] rounded-xl flex-shrink-0 pt-2 '>
+        <div className='flex justify-between px-5 items-center'>
+          <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
+          <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
+        </div>
+
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
+      </div>
+
+      <div className='h-full w-[300px] bg-[#fb5607] rounded-xl flex-shrink-0 pt-2 '>
+        <div className='flex justify-between px-5 items-center'>
+          <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
+          <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
+        </div>
+
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
+      </div>
+
+      <div className='h-full w-[300px] bg-[#8338ec] rounded-xl flex-shrink-0 pt-2 '>
+        <div className='flex justify-between px-5 items-center'>
+          <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
+          <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
+        </div>
+
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
+      </div>
+
+      <div className='h-full w-[300px] bg-[#ff99c8] rounded-xl flex-shrink-0 pt-2 '>
+        <div className='flex justify-between px-5 items-center'>
+          <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
+          <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
+        </div>
+
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
+      </div>
+
+      <div className='h-full w-[300px] bg-[#83c5be] rounded-xl flex-shrink-0 pt-2 '>
+        <div className='flex justify-between px-5 items-center'>
+          <h3 className='bg-red-700 px-3 font-extrabold rounded-sm text-white'>High</h3>
+          <h3 className='font-extrabold text-white'>26 Aug 2025</h3>
+        </div>
+
+        <h2 className='text-3xl mt-5 p-5 font-bold'>Complete emc project</h2>
+        <h3 className='  px-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, est.</h3>
+      </div>
       
     </div>
   )
